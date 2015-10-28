50b39935c4ffb76697448272378d7057e876edc9
Simon Christensen <eaasichr4@students.eaaa.dk>
2015-10-28 14:28:58 +0100

    caching php added

---
+++ caching.php
@@ -0,0 +1,9 @@
+<?php
+
+$url = "http://www.odaa.dk/api/action/datastore_search?resource_id=2a82a145-0195-4081-a13c-b0e587e9b89c";
+$json = file_get_contents($url);
+$json_data = json_decode($json, true);
+echo $json;
+
+file_put_contents('parking.json', json_encode($json));
+?>
